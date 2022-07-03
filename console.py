5fc7155fb54c8bdb64f9c152e1a13e1c8bd08af0
DevPacho <frankjose2004@gmail.com>
2022-07-02 20:25:55 -0500

    [FIXED] second approach to 'the_console' program

--- console.py
+++ console.py
@@ -2,7 +2,8 @@
 """Program that contains the entry point of the command interpreter"""
 
 import models
-import cmd, sys
+import cmd
+
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
@@ -10,10 +11,17 @@ class HBNBCommand(cmd.Cmd):
     def do_quit(self, arg):
         """Quit command to exit the program"""
         exit(0)
+        """return True"""
 
     def do_EOF(self, arg):
         """EOF command to exit the program"""
         exit(0)
+        """return True"""
+
+    def emptyline(self, arg):
+        """Called when an empty line is entered in response to the prompt."""
+        pass
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
