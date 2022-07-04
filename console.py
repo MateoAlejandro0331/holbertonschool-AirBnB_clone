0f80201c449fb0869d93f71d5e90df8cdb5dc302
DevPacho <frankjose2004@gmail.com>
2022-07-04 12:16:13 -0500

    [UPDATED] console program

--- console.py
+++ console.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 """Program that contains the entry point of the command interpreter"""
 
+
+from models.base_model import BaseModel
 import models
 import cmd
 
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
+
+    classes = {"BaseModel": BaseModel}
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
@@ -21,6 +25,18 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self, arg):
         """Called when an empty line is entered in response to the prompt."""
         pass
+
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel"""
+
+        arg = arg.split()
+
+        if len(arg) == 0:
+            print("** class name missing **")
+            return
+
+        if arg[0] in HBNBCommand.classes.keys():
+            new_object = BaseModel()
 
 
 if __name__ == '__main__':
