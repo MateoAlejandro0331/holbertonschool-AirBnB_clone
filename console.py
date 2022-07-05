ca34f7d81dd4e610f02239b42f66d4af710ef272
DevPacho <frankjose2004@gmail.com>
2022-07-05 12:05:02 -0500

    [CHANGED] return value

--- console.py
+++ console.py
@@ -20,13 +20,11 @@ class HBNBCommand(cmd.Cmd):
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
-        exit(0)
-        """return True"""
+        return True
 
     def do_EOF(self, arg):
         """EOF command to exit the program"""
-        exit(0)
-        """return True"""
+        return True
 
     def emptyline(self, arg):
         """Called when an empty line is entered in response to the prompt."""
