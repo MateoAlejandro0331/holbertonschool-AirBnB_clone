258ffcd8d2d2f5b7f66e970ad425bacbe4f2ea3d
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-07-05 16:29:33 -0500

    adding new base model

--- models/base_model.py
+++ models/base_model.py
@@ -1,54 +1,48 @@
 #!/usr/bin/python3
 """
-Main 'BaseModel' class that defines all common
+BaseModel class that defines all common
 attributes/methods for other classes
 """
 
 import uuid
-import models
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
-    """ Base class constructor method """
+    """ BaseModel class """
 
     def __init__(self, *args, **kwargs):
-        """ Base class initializes the objects """
-
+        """ Initial methods """
         if kwargs:
-            for key_inside, value_inside in kwargs.items():
-                if key_inside != "__class__":
-                    setattr(self, key_inside, value_inside)
-
-                format = "%Y-%m-%dT%H:%M:%S.%f"
-                if key_inside == "created_at":
-                    value_inside = datetime.strptime(value_inside, format)
-
-                if key_inside == "updated_at":
-                    value_inside = datetime.strptime(value_inside, format)
-
+            for key, value in kwargs.items():
+                if key == "created_at":
+                    self.created_at = datetime.fromisoformat(value)
+                if key == "updated_at":
+                    self.updated_at = datetime.fromisoformat(value)
+                if key not in ["__class__", "updated_at", "created_at"]:
+                    setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
+            storage.new(self)
 
     def __str__(self):
-        """ Method that returns a string representation """
-
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        """ return the string representation of the class instance """
+        string_str = "[" + self.__class__.__name__ + "] "
+        string_str += "({}) {}".format(self.id, self.__dict__)
+        return string_str
 
     def save(self):
-        """ Method that update the current date and time """
-
+        """ serializes __objects to the JSON file """
         self.updated_at = datetime.now()
-        models.storage.save()
+        storage.save()
 
     def to_dict(self):
-        """ Method that returns a dictionary representation """
-
-        self.__dict__["__class__"] = self.__class__.__name__
-        self.__dict__["created_at"] = self.created_at.isoformat()
-        self.__dict__["updated_at"] = self.updated_at.isoformat()
-
-        return self.__dict__
+        """ dictionary containing all keys/values of __dict__ """
+        dict_return = self.__dict__.copy()
+        dict_return["updated_at"] = self.updated_at.isoformat('T', 'auto')
+        dict_return["created_at"] = self.created_at.isoformat('T', 'auto')
+        dict_return["__class__"] = self.__class__.__name__
+        return dict_return
