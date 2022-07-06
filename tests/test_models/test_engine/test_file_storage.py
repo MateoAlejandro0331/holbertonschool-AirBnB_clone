81573a7a4a8a9c87d15de9142d92e893411f2392
DevPacho <frankjose2004@gmail.com>
2022-07-06 15:40:03 -0500

    [ADDED] more test cases for 'FileStorage' class

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -2,11 +2,29 @@
 """Tests for 'FileStorage' class with unittest module"""
 
 import unittest
+import models
+import json
+from os.path import exists
 from models.engine.file_storage import FileStorage
 
 
 class TestBaseModel(unittest.TestCase):
     """Testing our functions of 'FileStorage'"""
+
+    @classmethod
+    def setUpClass(cls):
+        """Method called before tests in an individual class are run"""
+        Model = FileStorage()
+
+    def test_doc_module(self):
+        """Testing all class documentation"""
+
+        self.assertTrue(models.engine.FileStorage.__doc__)
+        self.assertTrue(FileStorage.__doc__)
+        self.assertTrue(FileStorage.all.__doc__)
+        self.assertTrue(FileStorage.new.__doc__)
+        self.assertTrue(FileStorage.save.__doc__)
+        self.assertTrue(FileStorage.reload.__doc__)
 
     def test_is_instance(self):
         """Creating an instance of 'FileStorage'"""
@@ -28,6 +46,15 @@ class TestBaseModel(unittest.TestCase):
                     ({self.Model.id}) {self.Model.__dict__}"
         str2 = self.Model.__str__()
         self.assertEqual(str, str2)
+
+    def test_save(self):
+        """Testing the 'save' method"""
+
+        self.Model.save()
+        self.assertTrue(exists("file.json"))
+        with open("file.json") as file:
+            to_load = json.load(file)
+        self.assertTrue(self.Model.to_dict() in to_load.values())
 
 
 if __name__ == '__main__':
