505bc3aee216447c5528af3ea08fc8edce444fc1
DevPacho <frankjose2004@gmail.com>
2022-07-06 01:33:13 -0500

    [CHANGED] docstring length

--- models/user.py
+++ models/user.py
@@ -5,7 +5,7 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """Public class attributes"""
+    """Public class attributes to manage in 'User'"""
 
     email = ""
     password = ""
