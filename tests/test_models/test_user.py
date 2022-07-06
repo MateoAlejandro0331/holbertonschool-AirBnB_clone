c31983f8b3efcc8039d8c8f3a7c6653e0d23950e
DevPacho <frankjose2004@gmail.com>
2022-07-06 02:03:49 -0500

    [UPDATED] tests for 'User' class and [DELETED] Unnecessary module imports

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """Tests for 'User' class with unittest module"""
 
-import json
 import unittest
-from os.path import exists
 from models.user import User
 
 
@@ -30,21 +28,6 @@ class TestUser(unittest.TestCase):
                     ({self.Model.id}) {self.Model.__dict__}"
         str2 = self.Model.__str__()
         self.assertEqual(str, str2)
-
-    def test_save(self):
-        """Testing the 'save' method"""
-
-        self.Model.save()
-        self.assertTrue(exists("file.json"))
-        with open("file.json") as file:
-            to_load = json.load(file)
-        self.assertTrue(self.Model.to_dict() in to_load.values())
-
-    def test_to_dict(self):
-        """Testing the 'to_dict' method"""
-
-        dict = self.Model.to_dict()
-        self.assertEqual(self.Model.id, dict["id"])
 
 
 if __name__ == '__main__':
