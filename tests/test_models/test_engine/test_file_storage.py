92d4cb3a399b14d839e92f194572189e7ce0d882
DevPacho <frankjose2004@gmail.com>
2022-07-06 16:12:58 -0500

    [FIXED] tests output for 'FileStorage' class

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -5,6 +5,7 @@ import unittest
 import models
 import json
 from os.path import exists
+from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 
 
@@ -19,7 +20,7 @@ class TestBaseModel(unittest.TestCase):
     def test_doc_module(self):
         """Testing all class documentation"""
 
-        self.assertTrue(models.engine.FileStorage.__doc__)
+        self.assertTrue(models.engine.file_storage.__doc__)
         self.assertTrue(FileStorage.__doc__)
         self.assertTrue(FileStorage.all.__doc__)
         self.assertTrue(FileStorage.new.__doc__)
@@ -39,22 +40,16 @@ class TestBaseModel(unittest.TestCase):
         instance_1 = FileStorage()
         self.assertNotEqual(instance_0, instance_1)
 
-    def test_str(self):
-        """Testing the '__str__' method"""
-
-        str = f"[{self.Model.__class__.__name__}] \
-                    ({self.Model.id}) {self.Model.__dict__}"
-        str2 = self.Model.__str__()
-        self.assertEqual(str, str2)
-
     def test_save(self):
         """Testing the 'save' method"""
 
-        self.Model.save()
+        Model = BaseModel()
+
+        Model.save()
         self.assertTrue(exists("file.json"))
         with open("file.json") as file:
             to_load = json.load(file)
-        self.assertTrue(self.Model.to_dict() in to_load.values())
+        self.assertTrue(Model.to_dict() in to_load.values())
 
 
 if __name__ == '__main__':
