598f8854a0e33e1f861311696fca58e274783afc
DevPacho <frankjose2004@gmail.com>
2022-07-06 00:34:44 -0500

    [CHANGED] setattr for datetime objects

--- models/base_model.py
+++ models/base_model.py
@@ -17,15 +17,13 @@ class BaseModel:
 
         if kwargs:
             for key_inside, value_inside in kwargs.items():
-                if key_inside != "__class__":
+                if key_inside == "created_at" or key_inside == "updated_at":
+                    format = "%Y-%m-%dT%H:%M:%S.%f"
+                    datetime_object = datetime.strptime(value_inside, format)
+                    setattr(self, key_inside, datetime_object)
+
+                elif key_inside != "__class__":
                     setattr(self, key_inside, value_inside)
-
-                format = "%Y-%m-%dT%H:%M:%S.%f"
-                if key_inside == "created_at":
-                    value_inside = datetime.strptime(value_inside, format)
-
-                if key_inside == "updated_at":
-                    value_inside = datetime.strptime(value_inside, format)
 
         else:
             self.id = str(uuid.uuid4())
@@ -47,7 +45,7 @@ class BaseModel:
     def to_dict(self):
         """ Method that returns a dictionary representation """
 
-        new_dict = self.__dict__.copy()
+        new_dict = dict(self.__dict__)
         new_dict["__class__"] = self.__class__.__name__
         new_dict["created_at"] = self.created_at.isoformat()
         new_dict["updated_at"] = self.updated_at.isoformat()
