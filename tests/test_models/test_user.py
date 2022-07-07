e27f479eb591278f52b1d0589c95096702ae34b2
DevPacho <frankjose2004@gmail.com>
2022-07-07 12:16:20 -0500

    [ADDED] test for attributes of 'User' class

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -60,6 +60,26 @@ class TestUser(unittest.TestCase):
         dict = Model.to_dict()
         self.assertEqual(Model.id, dict["id"])
 
+    def test_instances_of_user(self):
+        """Testing the 'User' public attributes"""
+
+        Model = User()
+
+        self.assertTrue(hasattr(Model, "email"))
+        self.assertTrue(hasattr(Model, "password"))
+        self.assertTrue(hasattr(Model, "first_name"))
+        self.assertTrue(hasattr(Model, "last_name"))
+
+    def test_type_objects(self):
+        """Testing type of 'User' attributes (objects)"""
+
+        Model = User()
+
+        self.assertIsInstance(Model.email, str)
+        self.assertIsInstance(Model.password, str)
+        self.assertIsInstance(Model.first_name, str)
+        self.assertIsInstance(Model.last_name, str)
+
 
 if __name__ == '__main__':
     unittest.main()
